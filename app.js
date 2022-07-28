69804f1aaa53375b85854c6c918622885244ce8c
Grey Altr <grey.sifr@gmail.com>
2022-07-28 13:37:23 -0700

    uncomment out container block in app.js lines 10-12

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
 
-// const shedContainer = document.getElementById('shed-container');
-// const treeContainer = document.getElementById('tree-container');
-// const boulderContainer = document.getElementById('boulder-container');
+const shedContainer = document.getElementById('shed-container');
+const treeContainer = document.getElementById('tree-container');
+const boulderContainer = document.getElementById('boulder-container');
 
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
